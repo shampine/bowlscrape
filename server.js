b4ddba627e8c5153ac7e0d0934d9acf9b670fe6c
patrickshampine <patrick@patrickshampine.com>
2014-08-23 16:41:18 -0700

    Variable outline

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ var express = require('express');
 var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
-var app     = express();
+var app = express();
 
 app.get('/scrape', function(req, res){
 
-  //todo scrape
+  url = 'http://www.imdb.com/list/ls052535080/?start=1&view=detail&sort=listorian:asc';
+
+  request(url, function(error, response, html){
+
+    if(!error) {
+
+      var $= cheerio.load(html);
+      var title, year, rating, rank, url;
+      var json = { title : "", year : "", rating : "", rank : "", url : ""};
+
+    }
+  })
 
 })
 
